0ae3dd9971f8261534336bce3053e647ee7b8b1e
LiuWenYuan <15816141883@163.com>
2020-03-31 16:57:00 +0800

    input fixupgit add .! line height fixupgit add .!

--- sys.js
+++ sys.js
@@ -6,7 +6,7 @@ window.$ = function (selector) {
 }
 function _gen_wrapper(){
 	var libBullshit = window.bullshit;// require();
-	var 主题 = $('input').value;
+	var 主题 = $('#主题 input').value;
 	var 文章 = libBullshit(主题).split("\n");
 	let 排版 = '<div>' + 文章.join('</div><div>') + '</div>';
     $('#文章').innerHTML = 排版;
