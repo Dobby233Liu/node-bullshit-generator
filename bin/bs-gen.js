b9422811f118203f87f2168c9c18fea8e8599734
LiuWenYuan <15816141883@163.com>
2020-03-31 18:02:24 +0800

    useless info cleanup

--- bin/bs-gen.js
+++ bin/bs-gen.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
-
-/* bullshit-generator / bs-gen
- * orig py program: menzi11 https://github.com/menzi11/BullshitGenerator/blob/master/%E8%87%AA%E5%8A%A8%E7%8B%97%E5%B1%81%E4%B8%8D%E9%80%9A%E6%96%87%E7%AB%A0%E7%94%9F%E6%88%90%E5%99%A8.py
- * node.js port:	Dobby233Liu 2019/11/15 first broken version
- *			   					2020/01/14 v2
- * formatted by:	https://beautifier.io/ and js-beautify
- * @preserve
+/*
+ * bullshit-generator / bs-gen
+ * @author Dobby233Liu
  * @license Anti-996 License 1.0
- */
+*/
 var helpText = `bullshit-generator: Generates random Chinese bullshit.
 This is a work-in-progress help document.
 
