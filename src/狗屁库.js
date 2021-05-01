d52e1a85e143db2358be5087dd4bf2d930c43a69
Liu Wenyuan <15816141883@163.com>
2021-05-01 18:56:55 -0500

    ??????

--- src/狗屁库.js
+++ src/狗屁库.js
@@ -13,7 +13,7 @@ class 狗屁库 {
     }
     默认字典 = require("./dict/default/配置"); // obj with old params + 句号 etc.
     常用 = require("./常用");
-    随机 = new require("./随机常用")(Math.random);
+    随机 = new (require("./随机常用"))(Math.random);
     生成(主题 = "学生会退会", 长度 = 6000 * 主题.length, _字典 = this.默认字典(主题), _选项 = this.默认选项) {
         this.选项 = this.常用.合并对象(this.默认选项, _选项);
         this.字典 = this.常用.合并对象(this.默认字典(主题), _字典);
