e228c806d75b6ef97783e62d4f8e0e7b5222967f
Liu Wenyuan <15816141883@163.com>
2021-05-02 00:36:23 -0500

    aeth

--- src/dict/default-plus/配置.js
+++ src/dict/default-plus/配置.js
@@ -4,7 +4,7 @@ module.exports = (主题) => {
     return {
         "前面垫话": require("./前面垫话_aetherwu"), // much
         "后面垫话": require("./后面垫话_aetherwu"),
-        "废话": require("./废话_175")(主题), // best wording
+        "废话": require("./废话_aetherwu")(主题), // best wording
         "名人名言": require("./名人名言")(true), // fat
         "起段": "    ",
         "句号": "。"
