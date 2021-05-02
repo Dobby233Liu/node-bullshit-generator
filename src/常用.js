73d0bc7919b536580637d5074b27065e8aec33cd
Liu Wenyuan <15816141883@163.com>
2021-05-02 00:33:17 -0500

    VARRRRRR

--- src/常用.js
+++ src/常用.js
@@ -2,7 +2,7 @@
 // @author Dobby233Liu
 class 常用 {
     static 能否以句号结束(_tmp2) {
-        tmp2 = _tmp2.trim(); // temp postprocess
+        var tmp2 = _tmp2.trim(); // temp postprocess
         return !tmp2.endsWith("。") && !tmp2.endsWith(".") && !tmp2.endsWith("：") && !tmp2.endsWith(":") && !tmp2.endsWith("？") && !tmp2.endsWith("?") && !tmp2.endsWith("！") && !tmp2.endsWith("!") && !tmp2.endsWith(",") && !tmp2.endsWith("，");
     }
     static 合并对象(less, much) {
