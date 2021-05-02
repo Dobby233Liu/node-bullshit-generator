9a87d36171603c795bab9fe480c70a152b0c8932
Liu Wenyuan <15816141883@163.com>
2021-05-01 19:11:04 -0500

    Create 配置.js

---
+++ src/dict/default-plus/配置.js
@@ -0,0 +1,12 @@
+// You SHOULDN'T just use this, pick dicts on your own instead.
+// Comments below are on why I pick those dicts
+module.exports = (主题) => {
+    return {
+        "前面垫话": require("./前面垫话_aetherwu"), // much
+        "后面垫话": require("./后面垫话_aetherwu"),
+        "废话": require("./废话_175")(主题), // best wording
+        "名人名言": require("./名人名言"), // fat
+        "起段": "    ",
+        "句号": "。"
+    }
+}
