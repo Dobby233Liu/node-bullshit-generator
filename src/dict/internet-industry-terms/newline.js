277da7068a1c190defb6f4b4e8a6372335f6d385
Liu Wenyuan <15816141883@163.com>
2021-05-02 20:37:24 -0500

    clarify

--- src/dict/internet-industry-terms/newline.js
+++ src/dict/internet-industry-terms/newline.js
@@ -1,6 +1,6 @@
 module.exports = function newline(orig) {
     // From the original code we see that when the generator runs into a $
-    // and the paragraph is getting long, it adds a new paragraph.
+    // and the paragraph is getting long, it finishes the paragraph.
     // Tab characters are now hardcoded in config, so this doesn't matter.
     return orig + "\n" + "    ";
 }
