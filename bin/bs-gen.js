1dd8e0ed3cf4e054dad760afdef9ade44df04366
Liu Wenyuan <15816141883@163.com>
2021-08-02 13:35:47 +0800

    a

--- bin/bs-gen.js
+++ bin/bs-gen.js
@@ -1,33 +1,25 @@
 #!/usr/bin/env node
 
 /*
- * bullshit-generator / bs-gen CLI
+ * bullshit-generator / bs-gen (CLI)
  * @author Dobby233Liu
  * @license Anti-996 License 1.0
  */
-var helpText = `bullshit-generator: Generates random Chinese bullshit.
 
-Parameters: bullshit-generator [theme] [length] [dict]
-[theme] = Article/bullshit theme, as a string.
-[length] = Bullshit length. Must be a positive integer, or the value is ignored.
-[dict] = Path leading to a "dict config" file. Export must be a function.
-You can also specify a "-h" or "--help" in place of anywhere to display a help message (this)
+var helpText = `Generates random Chinese bullshit.
 
-Source code can be found at https://github.com/Dobby233Liu/node-bullshit-generator
+bullshit-generator [theme] [length] [dict]
+    theme: Article/bullshit theme (string)
+    length: Article/bullshit length (must be a positive integer, or script)
+    dict: Path leading to a "dict config" file. Export must be a function.
+You can also specify a "-h" or "--help" parameter in place of anywhere to display a help message (this)
+
+The source code can be found at https://github.com/Dobby233Liu/node-bullshit-generator
 
 This program is licensed using the Anti 996 License, version 1.0 (draft),
 inheriting the upstream's license.
-Obtain a copy at https://github.com/996icu/996.ICU/blob/617129b/LICENSE
+Obtain a copy at https://github.com/996icu/996.ICU/blob/617129b/LICENSE`
 
-THE LICENSED WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN ANY WAY CONNECTION WITH THE
-LICENSED WORK OR THE USE OR OTHER DEALINGS IN THE LICENSED WORK.
-
-This is a Node.js port of https://github.com/menzi11/BullshitGenerator`
 var argvClean = process.argv.map((x) => x.trim().toLowerCase())
 var theme, length, library, dict, article;
 if (argvClean.indexOf("--help") >= 0 || argvClean.indexOf("-h") >= 0) {
