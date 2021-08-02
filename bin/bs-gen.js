8f09688836997ea8238064d97619d8b53efed4ad
Liu Wenyuan <15816141883@163.com>
2021-08-02 12:48:28 +0800

    Update bs-gen.js

--- bin/bs-gen.js
+++ bin/bs-gen.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+console.log(require("../src/随机常用").洗牌遍历([1, 2, 3]).next());
+process.exit(1);
+
 /*
- * bullshit-generator / bs-gen
+ * bullshit-generator / bs-gen CLI
  * @author Dobby233Liu
  * @license Anti-996 License 1.0
  */
