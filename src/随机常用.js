c5e9831645e4b2c89bbdaafa3d91b3b69a46e3b3
Liu Wenyuan <15816141883@163.com>
2021-08-02 13:07:17 +0800

    Update 随机常用.js

--- src/随机常用.js
+++ src/随机常用.js
@@ -20,11 +20,11 @@ class 随机常用 {
     }
     static 洗牌遍历(数组, 重复度 = 1) {
         var 次数组 = [];
-        for (var ii = 0; ii < 重复度; i++) 次数组.concat(数组);
+        for (var ii = 0; ii < 重复度; ii++) 次数组.concat(数组);
         次数组 = this.洗牌(次数组);
         return {
             next: ((洗牌, _数组) => {
-                i = 0;
+                var i = 0;
                 var 数组 = _数组.slice();
                 return function __next__() {
                     console.log(i);
