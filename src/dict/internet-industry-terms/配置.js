8ef1f61af3c9a2d37e46d77160131ed0196d442c
Liu Wenyuan <15816141883@163.com>
2021-08-05 16:50:00 +0800

    [skip ci]

--- src/dict/internet-industry-terms/配置.js
+++ src/dict/internet-industry-terms/配置.js
@@ -1,10 +1,9 @@
-module.exports = (主题) => {
+module.exports = {
     return {
         "前面垫话": require("./前面垫话"),
         "后面垫话": require("./后面垫话"),
-        "废话": require("./废话")(主题),
+        "废话": require("./废话"),
         "名人名言": require("./名人名言"),
         "起段": "    ",
         "句号": "。"
     }
-}
