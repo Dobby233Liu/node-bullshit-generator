6b43e59b9febab33da98c9ddcadf0a53839210e3
Liu Wenyuan <15816141883@163.com>
2021-08-12 07:57:18 +0800

    Update 配置.js

--- src/dict/english/配置.js
+++ src/dict/english/配置.js
@@ -1,4 +1,5 @@
 const db = require("./db")
+const processer = require("./process")
 module.exports = {
     "废话": db["bullshits"],
     "名人名言": require("./名人名言"),
@@ -7,5 +8,8 @@ module.exports = {
     "后面垫话": db["suffix"],
     "addings": db["addings"],
     "examples": db["examples"],
-    "contrasts": db["contrasts"]
+    "contrasts": db["contrasts"],
+    "主题处理": processser["onTheme"],
+    "缓冲处理": processser["onSegment"],
+    "文章处理": processser["onArticle"],
 }
