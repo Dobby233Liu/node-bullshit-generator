811d57671a33e32b36c34cdcb06b25bc9714850b
Liu Wenyuan <15816141883@163.com>
2021-08-11 22:50:38 +0800

    [skip ci]

--- src/dict/english/process.js
+++ src/dict/english/process.js
@@ -6,11 +6,12 @@ roster["onTheme"] = function onTheme(theme)
 }
 
 const db = require("./db")
+const p2 = db["prefix_2"]
 let prefix2Machine = {}
 
 roster["onSegment"] = function onSegment(seg, dict, opt, useless, rng){
     if (seg.indexOf("{prefix2}") > -1) {
-        if (prefix2Machine == {}) prefix2Machine = rng.洗牌遍历(db["prefix_2"])
+        if (prefix2Machine.length <= 0) prefix2Machine = rng.洗牌遍历(p2)
         return prefix2Machine.next().value + seg.replace(/\{prefix2\}/g, "")
     }
     return seg
