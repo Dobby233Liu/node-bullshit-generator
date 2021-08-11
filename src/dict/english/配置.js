559c971d157b84b3d0bf84b2df051424e1cab132
Liu Wenyuan <15816141883@163.com>
2021-08-12 07:53:50 +0800

    Create 配置.js

---
+++ src/dict/english/配置.js
@@ -0,0 +1,12 @@
+const db = require("./db")
+
+module.exports = {
+    "废话": db["bullshits"],
+    "名人名言": require("./名人名言"),
+    "前面垫话": db["prefix"],
+    "prefix_2": db["prefix_2"],
+    "后面垫话": db["suffix"],
+    "addings": db["addings"],
+    "examples": db["examples"],
+    "contrasts": db["contrasts"]
+}
