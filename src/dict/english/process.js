7c2e9a034cbe01f4331c9b78375d55e9ff5e656d
Liu Wenyuan <15816141883@163.com>
2021-08-12 12:14:51 +0800

    Update process.js

--- src/dict/english/process.js
+++ src/dict/english/process.js
@@ -20,24 +20,23 @@ function ohNoes(arr, opt, rng) {
 }
 roster["onSegment"] = function onSegment(seg, dict, opt, useless, rng, type) {
     var lseg = seg
-    if (lseg.includes("{prefix_2}")) {
-        if (!prefix2Machine) prefix2Machine = ohNoes(dict["prefix_2"], opt, rng)
-        lseg = prefix2Machine.next().value + lseg.replace(/\{prefix_2\}/g, "")
-        console.log(lseg)
-    }
     if (type == "名人名言") {
         if (!examplesMachine) examplesMachine = ohNoes(dict["examples"], opt, rng)
-        lseg = examplesMachine.next().value + lseg
+        if (lseg.includes("{prefix_2}")) {
+            if (!prefix2Machine) prefix2Machine = ohNoes(dict["prefix_2"], opt, rng)
+            return examplesMachine.next().value + prefix2Machine.next().value + lseg.replace(/\{prefix_2\}/g, "")
+        }else {return examplesMachine.next().value + lseg
+        }
     } else if (type == "废话") {
         if ((Math.floor(Math.random() * 100) - 20) <= 45) {
             if (!addingsMachine) addingsMachine = ohNoes(dict["addings"], opt, rng)
-            lseg = addingsMachine.next().value + lseg
+            return addingsMachine.next().value + lseg
         } else {
             if (!contrastsMachine) contrastsMachine = ohNoes(dict["contrasts"], opt, rng)
-            lseg = contrastsMachine.next().value + lseg
+            return contrastsMachine.next().value + lseg
         }
     }
-    return lseg
+    return seg
 }
 
 function sentences(str) {
