5d8ecb3d69db7cfc7fdd8dd983cb86f75a58b273
Liu Wenyuan <15816141883@163.com>
2021-08-12 12:19:14 +0800

    Update 狗屁库.js

--- src/狗屁库.js
+++ src/狗屁库.js
@@ -54,7 +54,10 @@ class 狗屁库 {
                 缓冲 = this.下一句废话.next().value.replace(/\{主题\}/g, 主题);
                 类型 = "废话";
             } // else 缓冲 = "";
-            if (this.选项["允许字典处理字符串"] && this.字典["缓冲处理"] && typeof this.字典["缓冲处理"] == "function") 缓冲 = this.字典["缓冲处理"](缓冲, this.字典, this.选项, this.常用, this.随机, 类型);
+            if (this.选项["允许字典处理字符串"] && this.字典["缓冲处理"] && typeof this.字典["缓冲处理"] == "function") {
+                缓冲 = this.字典["缓冲处理"](缓冲, this.字典, this.选项, this.常用, this.随机, 类型);
+                console.log(缓冲);
+            }
             文章 += 缓冲;
         }
         缓冲 = "";
