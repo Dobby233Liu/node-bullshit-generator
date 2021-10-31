a87514b2be12dcbe5ae5d62f19ece24b9c6d1f88
Liu Wenyuan <15816141883@163.com>
2021-10-31 13:41:35 +0800

    [skip ci]

--- src/狗屁库.js
+++ src/狗屁库.js
@@ -4,15 +4,19 @@
  * @license Anti-996 License 1.0
  */
 class 狗屁库 {
+    constructor() {}
+
     默认选项 = {
         "有概率不添加后面垫话": false,
         "v3语法改进": true,
         "menzi11#175": true,
-        "允许字典处理字符串": true
+        "允许字典处理字符串": true,
+        "返回段落列表": false
     }
     默认字典 = require("./dict/default/配置");
     常用 = require("./常用");
     随机 = require("./随机常用");
+    
     生成(主题 = "学生会退会", 长度 = 6000 * 主题.length, _字典 = this.默认字典, _选项 = this.默认选项) {
         this.选项 = this.常用.合并对象(_选项, this.默认选项);
         this.字典 = this.常用.合并对象(_字典, this.默认字典);
@@ -27,16 +31,16 @@ class 狗屁库 {
         this.下一句名人名言 = this.随机.洗牌遍历(this.字典.名人名言);
         this.下一句废话 = this.随机.洗牌遍历(this.字典.废话);
         // ------------
-        var 文章 = this.起段.replace(/\{主题\}/g, 主题);
-        var 缓冲 = "";
+        var 文章 = [];
+        var 起段 = this.起段.replace(/\{主题\}/g, 主题);
+        var 段缓冲 = "";
         var 类型 = "忽略";
-        写文: while (文章.length < 长度 || (this.选项.v3语法改进 && (文章.trim().endsWith("：") || 文章.trim().endsWith(":") || 文章.trim().endsWith(",") || 文章.trim().endsWith("，")))) {
-            缓冲 = "";
+        while (文章.length < 长度) { // || (this.选项.v3语法改进 && (文章.trim().endsWith("：") || 文章.trim().endsWith(":") || 文章.trim().endsWith(",") || 文章.trim().endsWith("，")))) {
+            var 缓冲 = "";
             类型 = "忽略";
             let 分支 = Math.floor(Math.random() * 100);
-            if (分支 < 5 && ((!this.选项.v3语法改进) || !(文章.endsWith(this.起段) || 文章.trim().endsWith(",") || 文章.trim().endsWith("，")))) {
-                if (!this.选项.v3语法改进 || this.常用.能否以句号结束(文章.trim())) 缓冲 = this.字典.句号.replace(/\{主题\}/g, 主题);
-                缓冲 += "\n" + this.起段.replace(/\{主题\}/g, 主题);
+            if (分支 < 5 && ((!this.选项.v3语法改进) || !(段缓冲.length <= 0 || 段缓冲.trim().endsWith(",") || 段缓冲.trim().endsWith("，")))) {
+                if (!this.选项.v3语法改进 || this.常用.能否以句号结束(段缓冲.trim())) 缓冲 = this.字典.句号.replace(/\{主题\}/g, 主题);
                 类型 = "新行";
             } else if (分支 < 20) {
                 let 言 = this.下一句名人名言.next().value;
@@ -54,15 +58,20 @@ class 狗屁库 {
             } // else 缓冲 = "";
             // hack
             if (this.选项["允许字典处理字符串"] && this.字典["缓冲处理"] && typeof this.字典["缓冲处理"] == "function") {
-                文章 += this.字典["缓冲处理"](缓冲, this.字典, this.选项, this.常用, this.随机, 类型);
+                段缓冲 += this.字典["缓冲处理"](缓冲, this.字典, this.选项, this.常用, this.随机, 类型);
             }
-            else 文章 += 缓冲;
+            else 段缓冲 += 缓冲;
+            if (this.能加段(段缓冲) || 类型 == "新行") {
+                文章 = this.加段(文章, 段缓冲);
+                段缓冲 = "";
+            }
         }
-        缓冲 = "";
+        if (段缓冲 != "") this.加段(文章, 段缓冲);
+        段缓冲 = "";
         if (this.选项["允许字典处理字符串"] && this.字典["文章处理"] && typeof this.字典["文章处理"] == "function") 文章 = this.字典["文章处理"](文章, this.字典, this.选项, this.常用, this.随机);
+        if (!this.选项["返回段落列表"]) 文章 = 文章.map(x => 起段 + x).join("\n");
         return 文章;
     }
     generate = this.生成;
-    constructor() {}
 }
 module.exports = 狗屁库;
