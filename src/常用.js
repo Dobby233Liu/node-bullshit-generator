f498c1da6d512449418de777ca5fe47303347810
Liu Wenyuan <15816141883@163.com>
2021-11-28 11:31:29 +0800

    Update 常用.js

--- src/常用.js
+++ src/常用.js
@@ -1,26 +1,45 @@
-// the utilty
+// 常用类
 // @author Dobby233Liu
 class 常用 {
-    static 是否以字符串结束(字符串, 列表) { // endsWith
-        var ret = false;
-        for (var i of 列表)
+    static 是否以字符串结束(字符串, 检测列表) {
+        for (var i of 检测列表){
             if (字符串.endsWith(i)) {
-                ret = true
+                return true
                 break
-            }
-        return ret
+            }}
+        return false
     }
     static 能否以句号结束(段落) {
         return !this.是否以字符串结束(段落.trim(), ["。", ".", "：", ":", "？", "?", "！", "!", ",", "，", "、"]);
     }
-    static 合并对象(less, much) {
-        var ret = much;
-        for (var i in less) {
+    static 合并对象(镜像, 对象) {
+        var 合成 = 对象;
+        for (var i in 镜像) {
             if (i != "__proto__") {
-                ret[i] = less[i];
+                合成[i] = 镜像[i];
             }
         }
-        return ret;
+        return 合成;
+    }
+
+    // 分段系统实用函数
+    static 能加段(段) {
+        return 段.indexOf("\n") > 0;
+    }
+    static 加段(列表, 段) {
+        var 分 = 段.trimEnd().split("\n");
+        for (var i in 分) {
+            列表.push(分[i]);
+        }
+        return 列表;
+    }
+    static 最后一项(列表) {
+        return 列表[列表.length - 1];
+    }
+    static 总长(列表) {
+        var 长 = 0;
+        for (var i in 列表) 长 += 列表[i].length;
+        return 长;
     }
 }
 module.exports = 常用;
