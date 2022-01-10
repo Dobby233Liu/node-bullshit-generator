b1daee4206d06e9d9dd5745db3b70d4de02f73dc
Liu Wenyuan <15816141883@163.com>
2022-01-10 17:31:14 +0800

    simplify

--- src/随机常用.js
+++ src/随机常用.js
@@ -28,21 +28,17 @@ class 随机常用 {
         for (var ii = 0; ii < 重复度; ii++) 次数组 = 次数组.concat(数组);
         次数组 = this.洗牌(次数组);
         return {
-            next: (function (随机, _数组) {
-                var i = 0;
-                var 数组 = _数组.slice();
-                return function __next__() {
-                    const ret = 数组[i++];
-                    if (!(i < 次数组.length)) {
-                        数组 = 随机.洗牌(数组);
-                        i = 0;
-                    }
-                    return {
-                        value: ret,
-                        done: false,
-                    };
+            next: function __next__() {
+                const ret = 次数组[i++];
+                if (!(i < 次数组.length)) {
+                    次数组 = 随机.洗牌(次数组);
+                    i = 0;
+                }
+                return {
+                    value: ret,
+                    done: false,
                 };
-            })(this, 次数组)
+            }
         };
     }
     static 取随机数(最大数) {
