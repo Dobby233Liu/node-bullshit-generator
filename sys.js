1657c59e19d316ddb7382697920c42360b6420a7
Liu Wenyuan <15816141883@163.com>
2022-07-02 18:49:05 +0800

    fix for 1.4.0

--- sys.js
+++ sys.js
@@ -1,72 +1,78 @@
 window._config_ = {
-	historyApi: true,
-	errorHndl: true
+    historyApi: !!history,
+    errorHndl: true,
 };
+
 function 获取网址参数(参数) {
     return new URL(window.location.href).searchParams.get(参数);
 }
-function appendParamToUrlString(name, cnt, url = window.location.href){
-    var fussyURL = new URL(url);
-	if (!fussyURL.searchParams.has(name)) {
-		fussyURL.searchParams.append(name, cnt);
-	} else {
-		fussyURL.searchParams.set(name, cnt);
-	}
-	return fussyURL.href;
+function appendParamToUrlString(name, cnt, url = window.location.href) {
+    let fussyURL = new URL(url);
+    if (!fussyURL.searchParams.has(name)) {
+        fussyURL.searchParams.append(name, cnt);
+    } else {
+        fussyURL.searchParams.set(name, cnt);
+    }
+    return fussyURL.href;
 }
-window.$ = function (selector) {
+
+const $ = function (selector) {
     return document.querySelector(selector);
+};
+
+function appendArticle(文章) {
+    for (i in 文章) {
+        let newOwn = document.createElement("div");
+        newOwn.innerHTML = 文章[i];
+        $("#文章").appendChild(newOwn);
+    }
 }
-if(!window.bullshitClass) window.bullshitClass = new(window.bullshit)(); // allow hijacking
-function wArtAppender(文章){
-	for (i in 文章){
-		let newOwn = document.createElement('div');
-		newOwn.innerHTML = 文章[i];
-		$('#文章').appendChild(newOwn);
-	}
-}
-window.dontAddFirstHR = false;
-window.rootTitle = document.title + "";
 function insertAfter(newNode, referenceNode) {
-	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
-function _gen_wrapper(__主题){
-	var appendedFirstHR = false, yetAppendLastHR = false;
-	try {
-		if (__主题) $('#主题 input').value = __主题;
-		var 主题 = $('#主题 input').value;
-		var 文章;
-		文章 = window.bullshitClass.生成(主题).split("\n");
-		if (!window.dontAddFirstHR) {
-			$('#文章').appendChild(document.createElement("hr"));
-			appendedFirstHR = true;
-			window.dontAddFirstHR = true;
-		}
-		var legHero = document.createElement("h2");
-		legHero.innerHTML = escapeHtml(主题);
-		$('#文章').appendChild(legHero);
-		wArtAppender(文章);
-		yetAppendLastHR = true;
-		$('#文章').appendChild(document.createElement("hr"));
-		var composedTitle = document.title = window.rootTitle + " - 主题：" + 主题 + " - 请勿将生成的文章用于正规用途";
-		if (window._config_.historyApi) {
-			var elState = { 主题: 主题 };
-			history.pushState(elState, document.title, appendParamToUrlString("主题", 主题));
-		}
-		$('#文章').classList.remove("有错误");
-	} catch (ex) {
-		if (window._config_.errorHndl){
-			try { 
-				文章 = (ex.message + "\n\n" + (ex.stack || ex.stacktrace || "[无法获取堆栈跟踪]")).split("\n");
-				$('#文章').classList.add("有错误");
-				if (!window.dontAddFirstHR && !appendedFirstHR) $('#文章').appendChild(document.createElement("hr"));
-				wArtAppender(文章);
-				if (!yetAppendLastHR) $('#文章').appendChild(document.createElement("hr"));
-			} catch (e) { console.error(e); }
-		}
-		console.error(ex);
-	}
+
+const rootTitle = document.title;
+const bullshitClass = new window.bullshit();
+
+function _gen_wrapper(__主题) {
+    try {
+        if (__主题) $("#主题 input").value = __主题;
+        let 主题 = $("#主题 input").value;
+        let 文章 = bullshitClass.生成(主题, 6000, {
+            返回段落列表: true,
+        });
+        $("#文章").appendChild(document.createElement("hr"));
+        let themeHeader = document.createElement("h2");
+        themeHeader.innerHTML = escapeHtml(主题);
+        $("#文章").appendChild(themeHeader);
+        appendArticle(文章);
+        document.title = `${rootTitle} - 主题：${主题} - 请勿将生成的文章用于正规用途`;
+        if (window._config_.historyApi) {
+            history.pushState(
+                { 主题: 主题 },
+                document.title,
+                appendParamToUrlString("主题", 主题)
+            );
+        }
+        $("#文章").classList.remove("有错误");
+    } catch (ex) {
+        if (window._config_.errorHndl) {
+            try {
+                文章 = `${ex.message}\n\n${
+                    ex.stack || ex.stacktrace || "[无法获取堆栈跟踪]"
+                }`.split("\n");
+                $("#文章").classList.add("有错误");
+                $("#文章").appendChild(document.createElement("hr"));
+                appendArticle(文章);
+                $("#文章").appendChild(document.createElement("hr"));
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        console.error(ex);
+    }
 }
-if (( _主题 = 获取网址参数('主题'))) {
+
+if ((_主题 = 获取网址参数("主题"))) {
     window.onload = () => _gen_wrapper(_主题);
 }
