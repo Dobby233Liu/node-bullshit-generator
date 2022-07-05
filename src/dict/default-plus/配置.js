0f0bcd83d6cba7ed59c832ece373fcf8954db57d
Liu Wenyuan <15816141883@163.com>
2022-07-05 14:53:42 +0800

    nuke qwas982

--- src/dict/default-plus/配置.js
+++ src/dict/default-plus/配置.js
@@ -4,7 +4,7 @@ module.exports = {
     前面垫话: require("./前面垫话_aetherwu"), // much
     后面垫话: require("./后面垫话_aetherwu"),
     废话: require("./废话"), // thing
-    名人名言: require("./名人名言")(), // fat
+    名人名言: require("./名人名言"), // fat
     起段: "    ",
     句号: "。",
 };
--- src/dict/qwas982/配置.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    废话: require("./废话"),
-    前面垫话: require("../classic/名人名言"),
-    废话: require("./废话"),
-    后面垫话: require("../classic/名人名言"),
-    起段: "",
-    句号: ".",
-};
